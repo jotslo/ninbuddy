2cb703ece9ab3a59cfd3464e547fda6d5269281b
Josh <me@joshl.io>
2022-12-09 02:52:21 +0000

    debug

--- pi/main.py
+++ pi/main.py
@@ -2,7 +2,14 @@ import nxbt
 
 nx = nxbt.Nxbt()
 
+adapters = nx.get_available_adapters()
+
+print(len(adapters))
+
 controller_index = nx.create_controller(nxbt.PRO_CONTROLLER)
+
+print("Connecting")
+
 nx.wait_for_connection(controller_index)
 
 print("Connected")
