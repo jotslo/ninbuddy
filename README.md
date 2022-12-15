da09b22583450dba52d360701b512bef9f784d95
Josh <me@joshl.io>
2022-12-15 04:40:27 +0000

    nxbt integration test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ninbuddy
 Control your Nintendo Switch with a smartphone, or any controller.
+
+This is a WIP, and shows my current progress on the project. Feel free to disregard for now.
--- pi/main.py
+++ pi/main.py
@@ -6,37 +6,54 @@ import pygame
 import os
 import sys
 
+import nxbt
+
+import time
+
 """
 Button Map:
-0 - B
-1 - A
-2 - Y
-3 - X
-4 - L
-5 - R
-6 - ZL
-7 - ZR
-8 - Minus
-9 - Plus
+00 - B
+01 - A
+02 - Y
+03 - X
+04 - L
+05 - R
+06 - ZL
+07 - ZR
+08 - Minus
+09 - Plus
 10 - LStick
 11 - RStick
 12 - Home
 13 - Capture
 
-# not working?
-14 - DPad Up
-15 - DPad Down
-16 - DPad Left
-17 - DPad Right
-
+# Hat Map:
+(0,  1) - DPad Up
+(0, -1) - DPad Down
+(-1, 0) - DPad Left
+(1,  0) - DPad Right
 """
 
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 
+nx = nxbt.Nxbt()
 pygame.init()
 
-joystick = pygame.joystick.Joystick(0)
+controller_index = nx.create_controller(nxbt.PRO_CONTROLLER)
 
+print("Connecting")
+
+nx.wait_for_connection(controller_index)
+
+print("Connected")
+
+time.sleep(3)
+
+nx.press_buttons(controller_index, [nxbt.Buttons.B])
+
+print(nx.create_input_packet())
+
+joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
 while True:
@@ -50,62 +67,15 @@ while True:
         elif event.type == pygame.JOYBUTTONUP:
             print(f"{event.button} released")
         
-        print(joystick.get_hat(0))
-
-"""from inputs import get_gamepad
-
-while True:
-    events = get_gamepad()
-    for event in events:
-        print(event.ev_type, event.code, event.state)"""
-
-"""from pyjoystick.sdl2 import Key, Joystick, run_event_loop
+        #print(joystick.get_hat(0))
 
 
-def print_add(joy):
-    print("Added", joy)
-
-def print_remove(joy):
-    print("Removed", joy)
-
-def key_received(key):
-    print("Key:", key)
-
-run_event_loop(print_add, print_remove, key_received)"""
-
-
-"""devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-controller = None
-
-for device in devices:
-    if device.name == "Pro Controller":
-        controller = evdev.InputDevice(device.path)
-        break
-
-
-if controller:
-    while True:
-        event = controller.read_one()
-        if event:
-            print(event)"""
-
-"""nx = nxbt.Nxbt()
 
 adapters = nx.get_available_adapters()
 
 print(len(adapters))
 
-controller_index = nx.create_controller(nxbt.PRO_CONTROLLER)
 
-print("Connecting")
-
-nx.wait_for_connection(controller_index)
-
-print("Connected")
-
-time.sleep(3)
-
-nx.press_buttons(controller_index, [nxbt.Buttons.B])"""
 
 """port = 1
 passkey = "0000"
