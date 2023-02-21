146dcd471200a9803e1ebade42d1249e2ac5e739
Josh <me@joshl.io>
2023-02-21 03:06:51 +0000

    update host

--- pi/main.py
+++ pi/main.py
@@ -48,7 +48,7 @@ def create_controller():
     state = "Controller connected!"
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(host="0.0.0.0", port=8000)
 
 nx = nxbt.Nxbt()
 data.setup(nx)
