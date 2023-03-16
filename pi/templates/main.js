9b15323a4171b66314125aa3709f211529f00581
Josh <me@joshl.io>
2023-03-16 01:38:38 +0000

    working trigger buttons

--- pi/templates/main.js
+++ pi/templates/main.js
@@ -1,6 +1,12 @@
-const boundaries = {
-    "L": [[-200, 0], [150, 0], [150, 113], [-200, 113]],
-    "ZL": [[150, 0], [312, 0], [312, 86], [150, 86]]
+var inputs = {
+    "L": {"identifier": null,
+        "boundaries": [[-200, 0], [150, 0], [150, 113], [-200, 113]]},
+    "ZL": {"identifier": null,
+        "boundaries": [[150, 0], [312, 0], [312, 86], [150, 86]]},
+    "R": {"identifier": null,
+        "boundaries": [[1124, 0], [1500, 0], [1500, 113], [1124, 113]]},
+    "ZR": {"identifier": null,
+        "boundaries": [[958, 0], [1124, 0], [1124, 86], [958, 86]]}
 }
 
 function inside(point, vs) {
@@ -37,17 +43,96 @@ function absorbEvent(event) {
     event.returnValue = false;
 }
 
-function touchStart(event) {
-    const touch = event.touches[0];
-    const point = [1280 * touch.clientX / window.innerWidth, 637 * touch.clientY / window.innerHeight];
+function showInputs() {
+    document.getElementById("msg").textContent = "";
 
-    if (inside(point, boundaries["L"])) {
-        document.getElementById('msg').textContent = "L";
-    } else if (inside(point, boundaries["ZL"])) {
-        document.getElementById('msg').textContent = "ZL";
-    } else {
-        document.getElementById('msg').textContent = "NONE";
+    for (const key in inputs) {
+        if (inputs[key]["identifier"] != null) {
+            document.getElementById("msg").textContent += key + ", ";
+        }
     }
+}
+
+function getNewestTouches(event) {
+    var newTouches = [];
+
+    for (const touchKey in event.touches) {
+        const touch = event.touches[touchKey];
+        var isNewTouch = true;
+
+        for (const inputKey in inputs) {
+            const input = inputs[inputKey];
+
+            if (input["identifier"] == touch.identifier) {
+                isNewTouch = false;
+                break;
+            }
+        }
+
+        if (isNewTouch) {
+            newTouches.push(touch);
+        }
+    }
+
+    return newTouches;
+}
+
+function touchStart(event) {
+    const newTouches = getNewestTouches(event);
+
+    for (const touchKey in newTouches) {
+        const touch = newTouches[touchKey];
+        const point = [1280 * touch.clientX / window.innerWidth,
+            637 * touch.clientY / window.innerHeight];
+        
+        for (const key in inputs) {
+            if (inside(point, inputs[key]["boundaries"])) {
+                inputs[key]["identifier"] = touch.identifier;
+                break;
+            }
+        }
+    }
+
+    // debugging purposes
+    showInputs();
+
+    // prevent zooming, scrolling, selecting, etc.
+    event.returnValue = false;
+}
+
+/*function touchStart(event) {
+    for ()
+
+    const touch = event.touches[0];
+    const point = [1280 * touch.clientX / window.innerWidth,
+        637 * touch.clientY / window.innerHeight];
+    
+    for (const key in inputs) {
+        if (inside(point, inputs[key]["boundaries"])) {
+            inputs[key]["identifier"] = touch.identifier;
+            break;
+        }
+    }
+
+    showInputs();
+
+    // prevent zooming, scrolling, selecting, etc.
+    event.returnValue = false;
+}*/
+
+function touchEnd(event) {
+    for (const touchKey in event.changedTouches) {
+        const touch = event.changedTouches[touchKey];
+
+        for (const key in inputs) {
+            if (inputs[key]["identifier"] == touch.identifier) {
+                inputs[key]["identifier"] = null;
+                break;
+            }
+        }
+    }
+
+    showInputs();
 
     // prevent zooming, scrolling, selecting, etc.
     event.returnValue = false;
@@ -55,9 +140,9 @@ function touchStart(event) {
   
 let div1 = document.querySelector("#game-controller-button");
 div1.addEventListener("touchstart", touchStart);
-div1.addEventListener("touchend", absorbEvent);
 div1.addEventListener("touchmove", touchStart);
-div1.addEventListener("touchcancel", absorbEvent);
+div1.addEventListener("touchend", touchEnd);
+div1.addEventListener("touchcancel", touchEnd);
 
 /*var metaViewport = document.querySelector('meta[name="viewport"]');
 metaViewport.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, orientation=landscape');*/
