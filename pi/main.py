4616ef272b23b750b33a4414696eba5cd7ac7f4c
Josh <me@joshl.io>
2023-04-14 00:47:18 +0100

    remove reloader

--- pi/main.py
+++ pi/main.py
@@ -197,7 +197,7 @@ def input_packet(packet):
                 data.packet[button_name] = False"""
 
 if __name__ == '__main__':
-    threading.Thread(target=lambda: socketio.run(app, host="0.0.0.0", port="1234", debug=True, use_reloader=False)).start()
+    threading.Thread(target=lambda: socketio.run(app, host="0.0.0.0", port="1234", debug=True)).start()
 
 
 nx = nxbt.Nxbt()
