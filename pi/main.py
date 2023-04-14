2d8657bcac6a77c293f75055953ae1d80bcff42b
Josh <me@joshl.io>
2023-04-14 01:00:26 +0100

    port change?

--- pi/main.py
+++ pi/main.py
@@ -197,7 +197,7 @@ def input_packet(packet):
                 data.packet[button_name] = False"""
 
 if __name__ == '__main__':
-    threading.Thread(target=lambda: socketio.run(app, host="0.0.0.0", port="1234")).start()
+    threading.Thread(target=lambda: socketio.run(app, host="0.0.0.0", port="1233")).start()
 
 
 nx = nxbt.Nxbt()
