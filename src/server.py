47624d9f78d371e1c0e5a55d6665996a0f392ce1
Josh <me@joshl.io>
2023-04-18 19:26:28 +0100

    revert

--- src/server.py
+++ src/server.py
@@ -1,22 +1,135 @@
-
+from flask import Flask, render_template, jsonify, request
+from flask_socketio import SocketIO, emit
 import threading
 import pygame
+import nxbt
 import time
 import os
 import numpy
 import random
 
-from modules import data, functions, controller
-from modules.app import app, socketio
+from modules import data, functions
+
+global joystick
+
+app = Flask(__name__, static_url_path="/static")
+app.config["SECRET_KEY"] = "ninbuddy"
+socketio = SocketIO(app)
 
 # if video driver is not set, set it to dummy
 if "SDL_VIDEODRIVER" not in os.environ:
     os.environ["SDL_VIDEODRIVER"] = "dummy"
 
+@app.route("/")
+def dashboard():
+    return render_template("dashboard.html")
 
+@app.route('/ping-server')
+def get_data():
+    is_mobile = request.args.get("is_mobile", default = False) == "true"
+
+    if not is_mobile:
+        return jsonify({"message": data.state})
+
+    if not data.is_mobile_connected:
+        data.is_mobile_connected = True
+        print("User has connected!")
+
+        if data.controller == None:
+            threading.Thread(target=create_controller, args=(False,)).start()
+            return jsonify({"message": data.state})
+    
+    threading.Thread(target=track_last_ping).start()
+    return jsonify({"message": data.state})
+
+
+def update_packet(location, value):
+    if len(location) == 1:
+        data.packet[location[0]] = value
+    else:
+        data.packet[location[0]][location[1]] = value
+
+def update_joystick(joystick):
+    update_packet(["L_STICK", "X_VALUE"], joystick.get_axis(0) * 100)
+    update_packet(["L_STICK", "Y_VALUE"], joystick.get_axis(1) * -100)
+    update_packet(["R_STICK", "X_VALUE"], joystick.get_axis(3) * 100)
+    update_packet(["R_STICK", "Y_VALUE"], joystick.get_axis(4) * -100)
+
+def create_controller(is_real):
+    global joystick
+
+    data.state = "Connecting to console..."
+    data.is_real_controller = is_real
+
+    if is_real:
+        joystick = pygame.joystick.Joystick(0)
+        joystick.init()
+
+    data.controller = nx.create_controller(nxbt.PRO_CONTROLLER)
+    nx.wait_for_connection(data.controller)
+    data.state = "Connected to console!"
+
+def disconnected():
+    if not data.is_mobile_connected:
+        return
+
+    print("User has disconnected!")
+
+    if data.controller != None:
+        data.state = "Removing controller..."
+
+        try:
+            nx.remove_controller(data.controller)
+        except KeyError:
+            print("Controller removed during connection.")
+
+        data.controller = None
+    
+    data.is_mobile_connected = False
+    data.state = "Waiting for controller..."
+
+
+def track_last_ping():
+    data.last_mobile_ping = time.time()
+    time.sleep(5)
+
+    if time.time() - data.last_mobile_ping >= 5:
+        disconnected()
+
+
+@socketio.on("joystick-input")
+def joystick_input(packet):
+    packet["L_STICK"][0] = functions.clamp(packet["L_STICK"][0], -100, 100)
+    packet["L_STICK"][1] = functions.clamp(packet["L_STICK"][1], -100, 100)
+    packet["R_STICK"][0] = functions.clamp(packet["R_STICK"][0], -100, 100)
+    packet["R_STICK"][1] = functions.clamp(packet["R_STICK"][1], -100, 100)
+
+    update_packet(["L_STICK", "X_VALUE"], packet["L_STICK"][0])
+    update_packet(["L_STICK", "Y_VALUE"], -packet["L_STICK"][1])
+    update_packet(["R_STICK", "X_VALUE"], packet["R_STICK"][0])
+    update_packet(["R_STICK", "Y_VALUE"], -packet["R_STICK"][1])
+    
+    print("JOY", packet)
+
+@socketio.on("button-down")
+def button_down(packet):
+    update_packet([packet], True)
+    print("DOWN", packet)
+
+@socketio.on("button-up")
+def button_up(packet):
+    update_packet([packet], False)
+    print("UP", packet)
+
+if __name__ == '__main__':
+    threading.Thread(target=lambda: socketio.run(app, host="0.0.0.0", port="9000")).start()
+
+nx = nxbt.Nxbt()
+data.setup(nx)
+pygame.init()
 
 if pygame.joystick.get_count() >= 1:
-    controller.create(True)
+    create_controller(True)
 
 while True:
     if data.controller != None:
@@ -24,44 +137,44 @@ while True:
 
         if current_time - data.last_movement > 1/120:
             if data.is_real_controller:
-                controller.update_joystick(controller.joystick)
+                update_joystick(joystick)
                 data.last_movement = current_time
 
-            controller.nx.set_controller_input(data.controller, data.packet)
+            nx.set_controller_input(data.controller, data.packet)
 
     for event in pygame.event.get():
         if event.type == pygame.JOYDEVICEADDED and pygame.joystick.get_count() == 1:
             if data.controller == None:
-                controller.create(True)
+                create_controller(True)
         
         elif event.type == pygame.JOYDEVICEREMOVED and pygame.joystick.get_count() == 0:
-            controller.disconnected()
+            disconnected()
         
         elif event.type == pygame.JOYBUTTONDOWN:
-            controller.update_packet(data.button_map[event.button], True)
+            update_packet(data.button_map[event.button], True)
         
         elif event.type == pygame.JOYBUTTONUP:
-            controller.update_packet(data.button_map[event.button], False)
+            update_packet(data.button_map[event.button], False)
         
         elif event.type == pygame.JOYHATMOTION:
-            controller.update_packet(["DPAD_UP"], False)
-            controller.update_packet(["DPAD_DOWN"], False)
-            controller.update_packet(["DPAD_LEFT"], False)
-            controller.update_packet(["DPAD_RIGHT"], False)
+            update_packet(["DPAD_UP"], False)
+            update_packet(["DPAD_DOWN"], False)
+            update_packet(["DPAD_LEFT"], False)
+            update_packet(["DPAD_RIGHT"], False)
 
             if event.value[0] == 1:
-                controller.update_packet(["DPAD_RIGHT"], True)
+                update_packet(["DPAD_RIGHT"], True)
             elif event.value[0] == -1:
-                controller.update_packet(["DPAD_LEFT"], True)
+                update_packet(["DPAD_LEFT"], True)
             if event.value[1] == 1:
-                controller.update_packet(["DPAD_UP"], True)
+                update_packet(["DPAD_UP"], True)
             elif event.value[1] == -1:
-                controller.update_packet(["DPAD_DOWN"], True)
+                update_packet(["DPAD_DOWN"], True)
         
         elif event.type == pygame.JOYAXISMOTION:
             if event.axis == 2:
-                controller.update_packet(["ZL"], event.value == 1.0)
+                update_packet(["ZL"], event.value == 1.0)
             elif event.axis == 5:
-                controller.update_packet(["ZR"], event.value == 1.0)
+                update_packet(["ZR"], event.value == 1.0)
             
             data.cached_event = event
