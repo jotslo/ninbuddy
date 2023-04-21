1099b5c3ecfcd317e17d8cb31eb9f5fbb814cabd
Josh <me@joshl.io>
2023-04-21 18:47:24 +0100

    update some commenting

--- src/modules/controller.py
+++ src/modules/controller.py
@@ -1,45 +1,57 @@
 import nxbt, time
 
+# start nxbt and get format for input packet
+# input packet is sent to switch each frame
 nx = nxbt.Nxbt()
-
 packet = nx.create_input_packet()
-device = None
-input_devices = []
 
+# variables containing current controller and its visual state
+device = None
 state = "Waiting for controller..."
+
+# variables that determine connection state
 is_physical_connected = False
 is_mobile_connected = False
-
 is_disconnecting = False
 
+# update packet with new joystick values
 def update_packet(location, value):
     global packet
 
+    # if only one location, update packet with value
+    # otherwise, access nested dict and update with value
     if len(location) == 1:
         packet[location[0]] = value
     else:
         packet[location[0]][location[1]] = value
 
+# connect new generated controller to switch
 def connect():
     global joystick, state, device, input_devices, is_disconnecting
     
+    # if ready to connect, update states & connect via nxbt
     if not is_disconnecting:
         state = "Connecting to console..."
         device = nx.create_controller(nxbt.PRO_CONTROLLER)
         nx.wait_for_connection(device)
         state = "Connected to console!"
 
+# attempt to disconnect from switch, if possible
 def attempt_disconnect():
     global state, is_mobile_connected, is_physical_connected, device, input_devices, is_disconnecting
 
+    # if another device is still connected or is already disconnecting, ignore
     if is_mobile_connected or is_physical_connected or is_disconnecting:
         return
     
+    # set disconnecting state and wait for connection to be removed
     is_disconnecting = True
 
+    # while device isnt connected yet, wait
     while "Connected" not in state:
         time.sleep(0.1)
 
+    # if connected, disconnect from controller and update vars accordingly
     if device != None:
         state = "Disconnecting from console..."
         nx.remove_controller(device)
--- src/modules/web_handler.py
+++ src/modules/web_handler.py
@@ -8,71 +8,80 @@ import time
 # clamp a number between a min and max
 clamp = lambda n, _min, _max: max(min(n, _max), _min)
 
+# last time a mobile device pinged the server
 last_mobile_ping = 0
 
+# declare variables for web server
 app = Flask(__name__, static_url_path="/static")
 app.config["SECRET_KEY"] = "ninbuddy"
 socketio = SocketIO(app)
 
+# return the dashboard page upon visiting the root url
 @app.route("/")
 def dashboard():
     return render_template("dashboard.html")
 
+# mobile device pings this url each second for latest controller data
 @app.route('/ping-server')
 def get_data():
     is_mobile = request.args.get("is_mobile", default = False) == "true"
-    uuid = request.args.get("uuid")
 
+    # if the ping isn't from a mobile device, just return controller state
     if not is_mobile:
         return jsonify({"message": controller.state})
     
-    """if uuid not in controller.input_devices:
-        Thread(target=controller.connect, args=(uuid,)).start()
-        return jsonify({"message": controller.state})"""
-    
-    if not (controller.is_mobile_connected or controller.is_physical_connected):
+    # if no controller is connected, connect a new controller to switch
+    if not (controller.is_mobile_connected or controller.is_physical_connected or controller.is_disconnecting):
         controller.is_mobile_connected = True
         Thread(target=controller.connect).start()
         return jsonify({"message": controller.state})
     
-    Thread(target=track_last_ping, args=(uuid,)).start()
+    # otherwise, update the last ping time and return the controller state
+    Thread(target=track_last_ping).start()
     return jsonify({"message": controller.state})
 
-
-def track_last_ping(uuid):
+# update last mobile ping time each second
+def track_last_ping():
     global last_mobile_ping
     last_mobile_ping = time.time()
     time.sleep(5)
 
+    # if the last ping was more than 5 seconds ago, disconnect the controller
     if time.time() - last_mobile_ping >= 5:
         last_mobile_ping = time.time()
         controller.is_mobile_connected = False
         controller.attempt_disconnect()
 
-
+# forward joystick input to controller when received from mobile device
 @socketio.on("joystick-input")
 def joystick_input(packet):
+    # clamp joystick values between -100 and 100 to prevent errors
     packet["L_STICK"][0] = clamp(packet["L_STICK"][0], -100, 100)
     packet["L_STICK"][1] = clamp(packet["L_STICK"][1], -100, 100)
     packet["R_STICK"][0] = clamp(packet["R_STICK"][0], -100, 100)
     packet["R_STICK"][1] = clamp(packet["R_STICK"][1], -100, 100)
 
+    # update pending controller packet with joystick values
     controller.update_packet(["L_STICK", "X_VALUE"], packet["L_STICK"][0])
     controller.update_packet(["L_STICK", "Y_VALUE"], -packet["L_STICK"][1])
     controller.update_packet(["R_STICK", "X_VALUE"], packet["R_STICK"][0])
     controller.update_packet(["R_STICK", "Y_VALUE"], -packet["R_STICK"][1])
     
+    # output joystick values to console
     print("JOY", packet)
 
+# when button pressed, update pending controller packet
 @socketio.on("button-down")
 def button_down(packet):
     controller.update_packet([packet], True)
     print("DOWN", packet)
 
+# when button released, update pending controller packet
 @socketio.on("button-up")
 def button_up(packet):
     controller.update_packet([packet], False)
     print("UP", packet)
 
+# start web server on local network with port 8000
 def start():
     Thread(target=lambda: socketio.run(app, host="0.0.0.0", port="8000")).start()
