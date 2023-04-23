22e2982bd637d217bf2d09d77c9311abc9ba3f89
Josh <me@joshl.io>
2023-04-23 01:36:25 +0100

    code cleanup and more comments

--- src/modules/physical_handler.py
+++ src/modules/physical_handler.py
@@ -2,73 +2,100 @@ import pygame
 import time
 import os
 
-from modules import controller, data
+from modules import controller, input_maps
 
 # if video driver is not set, set it to dummy value
 # this allows us to use pygame without a display
 if "SDL_VIDEODRIVER" not in os.environ:
     os.environ["SDL_VIDEODRIVER"] = "dummy"
 
+# store last movement time & joystick object
 last_movement = 0
+joystick = None
 
-global joystick
-
+# update joystick values in packet
 def update_joystick():
-    global joystick
-    controller.update_packet(["L_STICK", "X_VALUE"], joystick.get_axis(0) * 100)
-    controller.update_packet(["L_STICK", "Y_VALUE"], joystick.get_axis(1) * -100)
-    controller.update_packet(["R_STICK", "X_VALUE"], joystick.get_axis(3) * 100)
-    controller.update_packet(["R_STICK", "Y_VALUE"], joystick.get_axis(4) * -100)
+    global last_movement, joystick
+    current_time = time.time()
 
+    # if 120th of a second has passed, update joystick values
+    if current_time - last_movement > 1/120:
+        controller.update_packet(["L_STICK", "X_VALUE"], joystick.get_axis(0) * 100)
+        controller.update_packet(["L_STICK", "Y_VALUE"], joystick.get_axis(1) * -100)
+        controller.update_packet(["R_STICK", "X_VALUE"], joystick.get_axis(3) * 100)
+        controller.update_packet(["R_STICK", "Y_VALUE"], joystick.get_axis(4) * -100)
+
+        # update last movement time
+        last_movement = current_time
+    
+    # update input packet sent to switch accordingly
+    controller.nx.set_controller_input(controller.device, controller.packet)
+
+# connect physical controller
 def connect_physical():
+    global joystick
+
+    # if physical controller is already connected, ignore
     if controller.is_physical_connected:
         return
-
+    
+    # initialise the connected physical controller
     joystick = pygame.joystick.Joystick(0)
     joystick.init()
     
+    # if mobile device isn't in-use, use physical controller
     if not controller.is_mobile_connected:
         controller.is_physical_connected = True
         controller.connect()
 
+# listen for physical controller input
 def listen():
     global last_movement, joystick
+
+    # initialise pygame for easy access to joystick input
     pygame.init()
 
+    # if physical controller is already connected, connect to switch
     if pygame.joystick.get_count() >= 1:
         connect_physical()
-
+    
+    # consistently check for input changes
     while True:
-        if controller.device != None:
-            current_time = time.time()
 
-            if current_time - last_movement > 1/120:
-                if controller.is_physical_connected:
-                    update_joystick(joystick)
-                    last_movement = current_time
+        # if physical controller is connected, update joystick each frame
+        if controller.device != None and controller.is_physical_connected:
+            update_joystick()
 
-                controller.nx.set_controller_input( controller.device, controller.packet)
-
+        # for each event in pygame event queue
         for event in pygame.event.get():
+
+            # if new physical controller is added for the first time, connect to switch
             if event.type == pygame.JOYDEVICEADDED and pygame.joystick.get_count() == 1:
                 connect_physical()
             
+            # if physical controller is removed, attempt to disconnect from switch
             elif event.type == pygame.JOYDEVICEREMOVED and pygame.joystick.get_count() == 0:
                 controller.is_physical_connected = False
                 controller.attempt_disconnect()
             
+            # if controller button is pressed, update packet accordingly
             elif event.type == pygame.JOYBUTTONDOWN:
-                controller.update_packet(data.button_map[event.button], True)
+                controller.update_packet(input_maps.button_map[event.button], True)
             
+            # if controller button is released, update packet accordingly
             elif event.type == pygame.JOYBUTTONUP:
-                controller.update_packet(data.button_map[event.button], False)
+                controller.update_packet(input_maps.button_map[event.button], False)
             
+            # if controller dpad is moved, update packet accordingly
             elif event.type == pygame.JOYHATMOTION:
+
+                # reset dpad values
                 controller.update_packet(["DPAD_UP"], False)
                 controller.update_packet(["DPAD_DOWN"], False)
                 controller.update_packet(["DPAD_LEFT"], False)
                 controller.update_packet(["DPAD_RIGHT"], False)
 
+                # update dpad values based on current values
                 if event.value[0] == 1:
                     controller.update_packet(["DPAD_RIGHT"], True)
                 elif event.value[0] == -1:
@@ -78,8 +105,12 @@ def listen():
                 elif event.value[1] == -1:
                     controller.update_packet(["DPAD_DOWN"], True)
             
+            # if controller trigger buttons are moved, update packet accordingly
+
+            # ZL & ZR are buttons unlike most controllers,
+            # so only apply user input if pressed past 75% of the way down
             elif event.type == pygame.JOYAXISMOTION:
                 if event.axis == 2:
-                    controller.update_packet(["ZL"], event.value == 1.0)
+                    controller.update_packet(["ZL"], event.value >= 0.75)
                 elif event.axis == 5:
-                    controller.update_packet(["ZR"], event.value == 1.0)
+                    controller.update_packet(["ZR"], event.value >= 0.75)
--- src/modules/static/js/client.js
+++ src/modules/static/js/client.js
@@ -1,21 +1,29 @@
+// declare env button, which covers screen to prevent scrolling
 const env = document.querySelector("#game-controller-button");
-const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
+// media query which determines whether orientation is portrait or landscape
 var mediaQuery = window.matchMedia("(orientation: portrait)");
 
-var readyForInput = false;
+// assigned to true or false, depending on whether the device is mobile
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+// socket.io connection for fast communication with server
 var socket = io();
 
+// variables that store input packets in various formats
+var lastJoystickInput = {}
 var inputPacket = {};
-
 var joystickInput = {
     "L_STICK": [0, 0],
     "R_STICK": [0, 0]
 }
 
-var lastJoystickInput = {}
+// determines whether user input can be processed
+// must receive a "connected" signal from server first
+var readyForInput = false;
 
 // input data & identifiers in order of priority
+// uses polygons as boundaries for each button, allowing for error tolerance
 var inputs = {
     "L": {"identifier": null, "touchpoint": [0, 0], "userinput": [0, 0],
         "boundaries": [[-200, 0], [150, 0], [150, 113], [-200, 113]]},
@@ -59,68 +67,118 @@ var inputs = {
         "boundaries": [[493, 241], [1095, 241], [1095, 800], [493, 800]]},
 }
 
-function generateUUID() { // Public Domain/MIT
-    // https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid
-    var d = new Date().getTime();//Timestamp
-    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16;//random number between 0 and 16
-        if(d > 0){//Use timestamp until depleted
-            r = (d + r)%16 | 0;
-            d = Math.floor(d/16);
-        } else {//Use microseconds since page-load if supported
-            r = (d2 + r)%16 | 0;
-            d2 = Math.floor(d2/16);
-        }
-        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-    });
-}
-
-const uuid = generateUUID();
-
+// determine whether touch point is within boundaries of button polygon
 function inside(point, vs) {
-    // ray-casting algorithm based on
+    // algorithm derived from:
+    // https://stackoverflow.com/questions/22521982/check-if-point-is-inside-a-polygon
     // https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html
     
+    // shorthand for point coordinates
     var x = point[0], y = point[1];
     
+    // variable to track whether point is inside polygon
     var inside = false;
+
+    // loop through each edge of polygon
     for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+        // get coordinates of edge
         var xi = vs[i][0], yi = vs[i][1];
         var xj = vs[j][0], yj = vs[j][1];
         
+        // check if point is on edge
         var intersect = ((yi > y) != (yj > y))
             && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        
+        // if point is on edge, update variable accordingly
         if (intersect) inside = !inside;
     }
     
+    // return whether point is inside polygon
     return inside;
 };
 
+// debug function that displays message in mobile browser
+// only used for debugging - not part of final product
 function debug(msg) {
     document.getElementById("msg").textContent = msg;
 }
 
+// function to set opacity of controls, so they can be hidden
+// setting to a really low value (not 0) prevents infinite page loading
 function setControlsOpacity(opacity) {
     const controls = document.getElementsByClassName("controls");
 
+    // for each control, set opacity to specified value
     for (var i = 0; i < controls.length; i++) {
         controls[i].style.opacity = opacity;
     }
 }
 
+// find input that matches identifier of touch point
+// this is used to determine which input to update when touch point moves
+function getInputFromIdentifier(identifier) {
+    for (const inputKey in inputs) {
+        const input = inputs[inputKey];
+
+        // if identifier matches a touch point, return that button name
+        if (input["identifier"] == identifier) {
+            return inputKey;
+        }
+    }
+
+    // if no match found, return null
+    return null;
+}
+
+// get all touch points that are not already being tracked
+function getNewestTouches(event) {
+    var newTouches = [];
+
+    // for each possible touch point
+    for (const touchKey in event.touches) {
+        const touch = event.touches[touchKey];
+        var isNewTouch = true;
+
+        // for each button type
+        for (const inputKey in inputs) {
+            const input = inputs[inputKey];
+
+            // if identifier matches a touch point, it's not new
+            // so don't add it to list of new touches
+            if (input["identifier"] == touch.identifier) {
+                isNewTouch = false;
+                break;
+            }
+        }
+
+        // touch is new, so add it to list of new touches
+        if (isNewTouch) {
+            newTouches.push(touch);
+        }
+    }
+
+    // return touches that are not already being tracked
+    return newTouches;
+}
+
+// send ping to server to get latest status each second
+// passes whether device is mobile so server can handle correctly
 function remainConnected() {
-    fetch('/ping-server?' + new URLSearchParams({"is_mobile": isMobile, "uuid": uuid}))
+    fetch('/ping-server?' + new URLSearchParams({"is_mobile": isMobile}))
         .then(response => response.json())
         .then(data => {
             const output = document.getElementById("centred-text");
 
+            // if connected to console & mobile device, show controls
             if (data.message == "Connected to console!" && isMobile) {
-                // if portrait
+
+                // if device is in portrait mode, tell user to rotate
                 if (mediaQuery.matches) {
                     output.textContent = "Rotate to landscape to play!";
-                    setControlsOpacity(0.001);
+                    setControlsOpacity(0.001); // hide controls (not 0 to prevent infinite page load)
                 }
+
+                // otherwise, show controls and allow input from user
                 else {
                     readyForInput = true;
                     output.textContent = "";
@@ -128,6 +186,7 @@ function remainConnected() {
                 }
             }
 
+            // if not connected to console, update output with current status
             else {
                 readyForInput = false;
                 output.textContent = data.message;
@@ -137,86 +196,49 @@ function remainConnected() {
     );
 }
 
-function updateDashboard() {
-    /*fetch('/data')
-        .then(response => response.json())
-        .then(data => {
-            const dashboardHeader = document.getElementById('msg');
-            dashboardHeader.textContent = data.message;
-        }
-    );*/
+// send input data to server to be forwarded to switch
+function sendInput() {
+    if (readyForInput) {
 
-    //socket.emit("get-state");
-}
-
-function showInputs() {
-    document.getElementById("msg").textContent = "";
-
-    for (const key in inputs) {
-        if (inputs[key]["identifier"] != null) {
-            document.getElementById("msg").textContent += key + ", ";
+        // only send input data if it has changed, to reduce server load
+        // send via socket & update last input sent for future comparisons
+        if (JSON.stringify(joystickInput) != lastJoystickInput) {
+            socket.emit("joystick-input", joystickInput);
+            lastJoystickInput = JSON.stringify(joystickInput);
         }
     }
 }
 
-function getInputFromIdentifier(identifier) {
-    for (const inputKey in inputs) {
-        const input = inputs[inputKey];
 
-        if (input["identifier"] == identifier) {
-            return inputKey;
-        }
-    }
-
-    return null;
-}
-
-function getNewestTouches(event) {
-    var newTouches = [];
-
-    for (const touchKey in event.touches) {
-        const touch = event.touches[touchKey];
-        var isNewTouch = true;
-
-        for (const inputKey in inputs) {
-            const input = inputs[inputKey];
-
-            if (input["identifier"] == touch.identifier) {
-                isNewTouch = false;
-                break;
-            }
-        }
-
-        if (isNewTouch) {
-            newTouches.push(touch);
-        }
-    }
-
-    return newTouches;
-}
-
+// function to handle initial touches
 function touchStart(event) {
-    const newTouches = getNewestTouches(event);
-
     // prevent zooming, scrolling, selecting, etc.
     event.returnValue = false;
 
+    // if not ready to take input, don't do anything
     if (!readyForInput) {
         return;
     }
 
+    // get list of new touches
+    const newTouches = getNewestTouches(event);
+
+    // for each newly detected touch, get coordinates
     for (const touchKey in newTouches) {
         const touch = newTouches[touchKey];
         const point = [1280 * touch.clientX / window.innerWidth,
             637 * touch.clientY / window.innerHeight];
         
+        // for each button, check if touch point is within boundaries
         for (const key in inputs) {
             if (inside(point, inputs[key]["boundaries"]) && inputs[key]["identifier"] == null) {
+
+                // if so, assign identifier and touchpoint so touch can be tracked
                 inputs[key]["identifier"] = touch.identifier;
                 inputs[key]["touchpoint"] = point;
 
-                //inputPacket[key] = true;
-
+                // if joystick is touched, move stick to the initial touch point
+                // this allows for better control of stick without looking at phone
                 if (key.includes("STICK")) {
                     const innerStick = document.getElementById(key + "-inner");
                     const outerStick = document.getElementById(key + "-outer");
@@ -227,44 +249,57 @@ function touchStart(event) {
                     innerStick.style.left = (point[0] / 12.8 - 5) + "%";
                     innerStick.style.top = (point[1] / 6.37 - 10) + "%";
                 }
+
+                // if not stick, send button down event to server immediately
+                // stick input updates 60 times per second separately
                 else {
                     socket.emit("button-down", key);
                 }
 
+                // stop checking for other buttons
                 break;
             }
         }
     }
 }
 
+// function to handle movement to existing touch points
 function touchMove(event) {
-    const activeTouches = Array.from(event.touches);
-
     // prevent zooming, scrolling, selecting, etc.
     event.returnValue = false;
 
+    // if not ready to take input, don't do anything
     if (!readyForInput) {
         return;
     }
     
+    // get list of all active touch points
+    const activeTouches = Array.from(event.touches);
+
+    // for each active touch point, get its stored info
     for (const touchKey in activeTouches) {
         const touch = event.touches[touchKey];
         const inputKey = getInputFromIdentifier(touch.identifier);
 
+        // if associated input is a joystick, get latest touch point
         if (inputKey) {
             if (inputKey.includes("STICK")) {
                 const input = inputs[inputKey];
                 const point = [1280 * touch.clientX / window.innerWidth,
                     637 * touch.clientY / window.innerHeight];
 
-                const innerStick = document.getElementById(inputKey + "-inner");
 
+                // move inner stick to touch point for visual feedback
+                const innerStick = document.getElementById(inputKey + "-inner");
                 innerStick.style.left = (point[0] / 12.8 - 5) + "%";
                 innerStick.style.top = (point[1] / 6.37 - 10) + "%";
 
+                // store joystick position based on touch point & initial touch point
+                // this is used to send joystick input to server
                 input["userinput"] = [point[0] - input["touchpoint"][0],
                     point[1] - input["touchpoint"][1]];
 
+                // add to joystick input object to be sent to server
                 joystickInput[inputKey] = input["userinput"];
             }
         }
@@ -272,24 +307,29 @@ function touchMove(event) {
 }
 
 function touchEnd(event) {
-    const endedTouches = Array.from(event.changedTouches);
-
     // prevent zooming, scrolling, selecting, etc.
     event.returnValue = false;
 
+    // if not ready to take input, don't do anything
     if (!readyForInput) {
         return;
     }
 
+    // get list of ended touches
+    const endedTouches = Array.from(event.changedTouches);
+
+    // for each ended touch, get its stored info
     for (const touchKey in endedTouches) {
         const touch = endedTouches[touchKey];
 
+        // if associated input is found, reset its info so it's considered released
         for (const key in inputs) {
             if (inputs[key]["identifier"] == touch.identifier) {
                 inputs[key]["identifier"] = null;
                 inputs[key]["touchpoint"] = [0, 0];
                 inputs[key]["userinput"] = [0, 0];
 
+                // if joystick, move stick back to center
                 if (key.includes("STICK")) {
                     joystickInput[key] = [0, 0];
 
@@ -302,55 +342,29 @@ function touchEnd(event) {
                     innerStick.style.left = key == "L_STICK" ? "8.5%" : "56%";
                     innerStick.style.top = key == "L_STICK" ? "25%" : "65%";
                 }
+
+                // otherwise, send button up event to server for immediate release
                 else {
                     socket.emit("button-up", key);
                 }
 
+                // stop checking for other buttons
                 break;
             }
         }
     }
 }
 
+// when page is fully loaded, call functions at each interval
+// sendInput - 60/sec - send input data to server to be forwarded to switch
+// remainConnected - 1/sec - ping server to get latest state & remain connected
+window.addEventListener('load', function() {
+    setInterval(sendInput, 1 / 60);
+    setInterval(remainConnected, 1000);
+});
+
+// when screen is touched in certain ways, call appropriate functions
 env.addEventListener("touchstart", touchStart);
 env.addEventListener("touchmove", touchMove);
 env.addEventListener("touchend", touchEnd);
 env.addEventListener("touchcancel", touchEnd);
-
-function sendInput() {
-    /*if (readyForInput) {
-        var inputPacket = {};
-
-        for (const key in inputs) {
-            inputPacket[key] = {};
-            inputPacket[key]["identifier"] = inputs[key]["identifier"] != null ? 1 : 0;
-            if (key.includes("STICK")) {
-                inputPacket[key]["userinput"] = inputs[key]["userinput"];
-            }
-        }
-
-        socket.emit("input-packet", inputPacket);
-    }*/
-
-    if (readyForInput) {
-        if (JSON.stringify(joystickInput) != lastJoystickInput) {
-            socket.emit("joystick-input", joystickInput);
-            lastJoystickInput = JSON.stringify(joystickInput);
-        }
-    }
-}
-
-// when get-state is received, update the dashboard
-socket.on("get-state", function(state) {
-    const output = document.getElementById("centred-text");
-    output.textContent = state;
-});
-
-socket.on("ready-for-input", function(state) {
-    readyForInput = state;
-});
-
-window.addEventListener('load', function () {
-    setInterval(sendInput, 1 / 60);
-    setInterval(remainConnected, 1000);
-});
