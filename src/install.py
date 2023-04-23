b6e62d936705ab342341f85a4b4453b45ae5b8e6
Josh <me@joshl.io>
2023-04-23 18:09:23 +0100

    file location fix

--- src/install.py
+++ src/install.py
@@ -6,39 +6,43 @@ import sys
 import subprocess
 
 download_url = "https://github.com/jotslo/ninbuddy/releases/download/v0.1-alpha/ninbuddy.zip"
-extract_dir = os.path.expanduser("~/ninbuddy")
-
-os.system("clear")
+extract_dir = f"/home/{os.environ.get('SUDO_USER')}/ninbuddy"
 
 red = "\033[31m"
 bold = "\033[1m"
 green = "\033[32m"
 reset = "\033[0m"
 
-print(f"{red}{bold}### NinBuddy by Josh Lotriet{reset}")
+def start():
+    os.system("clear")
+    print(f"{red}{bold}### NinBuddy by Josh Lotriet{reset}")
 
-if os.geteuid() != 0:
-    print(f"{red}{bold}ERROR: You must run this script as root.{reset}")
-    print(f"To fix, type 'sudo {sys.executable} install.py'")
-    sys.exit(1)
+    if os.geteuid() != 0:
+        print(f"{red}{bold}ERROR: You must run this script as root.{reset}")
+        print(f"To fix, type 'sudo {sys.executable} install.py'")
+        sys.exit(1)
 
-home_dir = os.environ["HOME"]
-extract_dir = extract_dir.replace("/root/", f"{home_dir}/")
+def get_source_files():
+    home_dir = os.environ["HOME"]
+    extract_dir = extract_dir.replace("/root/", f"{home_dir}/")
 
-print(f"{green}{bold}### PLEASE WAIT...{reset}\n")
-print("Downloading source files...")
+    print(f"{green}{bold}### PLEASE WAIT...{reset}\n")
+    print("Downloading source files...")
 
-with urllib.request.urlopen(download_url) as url_response:
-    with zipfile.ZipFile(io.BytesIO(url_response.read())) as zip_file:
-        zip_file.extractall(extract_dir)
+    with urllib.request.urlopen(download_url) as url_response:
+        with zipfile.ZipFile(io.BytesIO(url_response.read())) as zip_file:
+            zip_file.extractall(extract_dir)
 
-print(f"Source files downloaded to {os.path.abspath(extract_dir)}.")
+    print(f"Source files downloaded to {os.path.abspath(extract_dir)}.")
 
-print("Preparing to install NXBT and PyGame...")
 
-pip_path = sys.executable.replace("python", "pip")
-subprocess.run(["sudo", pip_path, "install", "git+https://github.com/Brikwerk/nxbt.git@dev"])
-subprocess.run([pip_path, "install", "pygame==2.1.2"])
+def install_dependencies():
+    print("Preparing to install NXBT and PyGame...")
+
+    pip_path = sys.executable.replace("python", "pip")
+    subprocess.run(["sudo", pip_path, "install", "git+https://github.com/Brikwerk/nxbt.git@dev"])
+    subprocess.run([pip_path, "install", "pygame==2.1.2"])
+
 
 def ask(question, custom_response=None, invalid_response=None):
     os.system("clear")
@@ -72,16 +76,25 @@ def ask(question, custom_response=None, invalid_response=None):
         return response.lower().startswith("y")
 
 
-auto_start = ask("Do you want NinBuddy to automatically start when your Pi turns on?")
-use_custom_port = ask("""Do you want to NinBuddy to use a port other than 1010?
-If you're not sure, type 'n'.""")
+def user_configuration():
+    auto_start = ask("Do you want NinBuddy to automatically start when your Pi turns on?")
+    use_custom_port = ask("""Do you want to change the default dashboard port? (1010)
+    If you're not sure, type 'n'.""")
 
-if use_custom_port:
-    port = ask("What port would you like NinBuddy to use?", custom_response=True)
+    if use_custom_port:
+        port = ask("What port would you like NinBuddy to use?", custom_response=True)
 
-print(auto_start)
-print(use_custom_port)
-print(port)
+    start_now = ask("Do you want to start NinBuddy now?")
+
+#####################################
+
+start()
+get_source_files()
+install_dependencies()
+user_configuration()
+
+
+
 
 # auto start on boot?
 # custom port? or default to 1010?
