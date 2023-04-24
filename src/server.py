72467858068edb29537c1d90a83d6243fa515ab9
Josh <me@joshl.io>
2023-04-24 17:04:15 +0100

    exit with error if not sudo

--- src/server.py
+++ src/server.py
@@ -1,9 +1,16 @@
 # main file to run on raspberry pi
-from os import environ
+from os import environ, geteuid
+from sys import executable, exit
 
 # prevent pygame from outputting extraneous info
 # this makes the console output cleaner
 environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+
+# declare colours for console output
+red = "\033[31m"
+bold = "\033[1m"
+green = "\033[32m"
+reset = "\033[0m"
 
 # if video driver is not set, set it to dummy value
 # this allows us to use pygame without a display
@@ -15,5 +22,12 @@ from modules import web_handler, physical_handler
 
 # if file is ran directly, start web server and listen for physical controller input
 if __name__ == "__main__":
+    # if user is not root, exit with error
+    if geteuid() != 0:
+        print(f"{red}{bold}### NinBuddy by Josh Lotriet{reset}")
+        print(f"{red}{bold}ERROR: You must run this script as root.{reset}")
+        print(f"To fix, type 'sudo {executable} install.py'")
+        exit(1)
+
     web_handler.start()
     physical_handler.listen()
